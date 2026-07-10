817e473e5395a9019e3bd00192bf6ed24bd8cb1e
unknown <nishantsinghrajput2612@gmail.com>
2026-07-10 20:07:18 +0530

    First Commit Of C Language

---
+++ mark53.c
@@ -0,0 +1,19 @@
+/*Program to swap two numbers without changing the original values 
+using function*/
+#include<stdio.h>
+void swap(int, int);
+int main()
+{
+    int x=10,y=20;
+    printf("Before swap: x=%d, y=%d\n",x,y);
+    swap(x,y);
+    printf("After swap: x=%d, y=%d\n",x,y);
+    return 0;
+}
+void swap(int a, int b)
+{
+    int c=a;
+    a=b;
+    b=c;
+    printf("Inside swap: a=%d, b=%d\n",a,b);
+}
