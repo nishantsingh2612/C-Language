b79d18dbf2b6a8a698a01561487784b499a989f7
unknown <nishantsinghrajput2612@gmail.com>
2026-07-19 23:00:00 +0530

    First Commit Of C Language

--- mark79.c
+++ mark79.c
@@ -1,5 +1,7 @@
 // 2-D Array of Characters
 #include<stdio.h>
+#include<string.h>
+
 int main()
 {
     // Declare and initialise a 2D array of characters
@@ -11,6 +13,14 @@ int main()
         // printf("%s", names[i]);
         // printf("\n");
         puts(names[i]);
+    }
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j < strlen(names[i]); j++)
+        {
+            printf("%c", names[i][j]);
+        }
+        printf("\n");
     }
     // Accessing and printing a specific character
     printf("The sixth character of the second name is: %c", names[1][5]);
